147549032cf324c9b8f500b64542f886f5d32b0f
cloudlessk <akwok63@gmail.com>
2022-09-09 07:17:26 +0000

    Added s3 bucket module

--- s3-buckets.tf
+++ s3-buckets.tf
@@ -2,8 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/tf-train63/s3-bucket/aws"
   version = "3.4.0"
 
-  bucket = "my-s3-bucket"
-  bucket_prefix=var.prefix
+  bucket = var.prefix
   acl    = "private"
 
   versioning = {
